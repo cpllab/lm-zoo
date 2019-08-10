bc2c97ecfd8036a83cfd2bee9a46e45beed9439d
Jon Gauthier <jon@gauthiers.net>
2019-08-10 16:51:21 +0000

    remove outdated scripts directory

--- scripts/eval_grnn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-GRNN_ROOT="/om/group/cpl/language-models/colorlessgreenRNNs"
-python ${GRNN_ROOT}/src/language_models/evaluate_target_word_test.py \
-    --data ${GRNN_ROOT}/data/wiki \
-    --checkpoint ${GRNN_ROOT}/hidden650_batch128_dropout0.2_lr20.0.pt \
-    --prefixfile $1 \
-    --outf $2 \
-    --surprisalmode True
--- scripts/eval_jrnn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-JRNN_ROOT="/om/group/cpl/language-models/jrnn"
-python ${JRNN_ROOT}/lm_1b/eval_test_google.py \
-    --pbtxt ${JRNN_ROOT}/data/graph-2016-09-10.pbtxt \
-    --ckpt '/om/group/cpl/language-models/jrnn/data/ckpt-*' \
-    --vocab_file ${JRNN_ROOT}/data/vocab-2016-09-10.txt \
-    --input_file $1 \
-    --output_file $2
