f3f8c7d9bd1f3d11432b212791e130257891afec
Jon Gauthier <jon@gauthiers.net>
2020-04-18 14:33:21 -0400

    add tools for building and distributing all images

---
+++ scripts/dist_all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+# Get all local cpllab/language-models image references
+image_refs=($(docker images -f reference=cpllab/language-models --format "{{.Repository}}:{{.Tag}}" ))
+
+# Push to Docker Hub
+for ref in ${image_refs[*]}; do
+    echo "======= Pushing", $ref
+    docker push $ref
+done
+
+# Build new registry
+python tools/build_registry.py ${image_refs[@]}
