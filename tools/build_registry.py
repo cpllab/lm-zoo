c92c5880b92599bca69f1496817ecd4030b1cf63
Jon Gauthier <jon@gauthiers.net>
2020-04-18 21:44:57 -0400

    build scripts ready, registry ready!

--- tools/build_registry.py
+++ tools/build_registry.py
@@ -15,29 +15,26 @@ from tqdm import tqdm
 L = logging.getLogger(__name__)
 
 
-# Remove these keys from specs when building the registry.
-REMOVE_KEYS = ["vocabulary"]
-
-
 def main(args):
     registry = {}
     client = docker.from_env()
 
     for docker_image in tqdm(args.docker_images):
         try:
-            image_spec = client.containers.run(docker_image, command="spec", remove=True,
+            # HACK: Remove vocabulary from spec on guest-side to avoid
+            # streaming huge amounts of data for each model. We'd delete it
+            # host-side, anyway ..
+            command = r"""bash -c "spec | python -c 'import json, sys; spec=json.load(sys.stdin); del spec[\"vocabulary\"]; json.dump(spec, sys.stdout);'" """
+            image_spec = client.containers.run(docker_image, command=command, remove=True,
                                                detach=False, stdout=True, stderr=False)
             image_spec = json.loads(image_spec)
         except Exception as e:
             L.error("Error fetching spec from image %s", docker_image, exc_info=e)
             continue
 
-        for key in REMOVE_KEYS:
-            del image_spec[key]
-
         registry[image_spec["name"]] = image_spec
 
-    json.dump(registry, sys.stdout)
+    json.dump(registry, sys.stdout, indent=2)
 
 
 if __name__ == "__main__":
