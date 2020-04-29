cfca57a3b4a7d689c5c2c65c83ffce24e05cf5e1
Jon Gauthier <jon@gauthiers.net>
2020-04-29 13:43:47 -0400

    update LM registry after multistage merge+dist

--- scripts/dist_all.sh
+++ scripts/dist_all.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Get all local cpllab/language-models image references
-image_refs=($(docker images -f reference=cpllab/language-models --format "{{.Repository}}:{{.Tag}}" ))
+image_refs=($(docker images -f reference=cpllab/language-models --format "{{.Repository}}:{{.Tag}}" | grep -v "<none>" | grep -v "\-base"))
 
 # Push to Docker Hub
 for ref in ${image_refs[*]}; do
