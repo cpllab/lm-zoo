eef06170a92bae05454c039154b149ba773ad727
Jon Gauthier <jon@gauthiers.net>
2020-05-01 13:01:32 -0400

    build_and_test: don't assume cpllab/language-models target

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 model_dirname=$1
-tag=$2
+target=$2
 
 COMMIT=$(git rev-parse --verify HEAD)
 CPL_SSH_PRV_KEY="$(cat ~/.ssh/id_rsa)"
@@ -11,9 +11,9 @@ model_dir=models/${model_dirname}
 sha_out=($(tar -c ${model_dir} | sha1sum))
 FILES_SHA1=$sha_out
 
-docker build -t cpllab/language-models:$tag -f ${model_dir}/Dockerfile\
+docker build -t $target -f ${model_dir}/Dockerfile\
         --build-arg COMMIT=$COMMIT \
         --build-arg FILES_SHA1=$FILES_SHA1 \
         --build-arg "CPL_SSH_PRV_KEY=${CPL_SSH_PRV_KEY}" \
         . \
-    && docker run --rm -v `pwd`/test.py:/test.py -v `pwd`/docs/schemas:/schemas cpllab/language-models:${tag} python /test.py
+    && docker run --rm -v `pwd`/test.py:/test.py -v `pwd`/docs/schemas:/schemas $target python /test.py
