transformers-base: fix softmax axis, yikes

models/transformers-base/get_surprisals.py
@@ -38,7 +38,7 @@ def eval_sentence(sentence, tokenizer, model, device):
     tokens_tensor = tokens_tensor.to(device)
 
     with torch.no_grad():
-        log_probs = model(tokens_tensor)[0].log_softmax(dim=1).numpy()
+        log_probs = model(tokens_tensor)[0].log_softmax(dim=2).numpy()
 
     # initial token gets surprisal 0
     surprisals = [0.0]